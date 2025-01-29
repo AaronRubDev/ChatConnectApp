f92cd4cc71d48fbe1b985d492c374ea867c4d018
Aaron <aaronrubidev@gmail.com>
2025-01-29 12:45:16 -0600

    Initial settings and ScreenSplash added

--- app/src/main/java/com/aaronrubidev/chatconnect/MainActivity.kt
+++ app/src/main/java/com/aaronrubidev/chatconnect/MainActivity.kt
@@ -1,20 +1,19 @@
 package com.aaronrubidev.chatconnect
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.aaronrubidev.chatconnect.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
     }
 }
+
